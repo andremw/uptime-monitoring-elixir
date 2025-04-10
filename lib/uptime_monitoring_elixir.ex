db9402169a252f750dcaf5b9ede5ddbed8798119
andremw <mazoni.andre@gmail.com>
2025-04-11 07:55:02 +0930

    recreated with --sup

---
+++ lib/uptime_monitoring_elixir.ex
@@ -0,0 +1,18 @@
+defmodule UptimeMonitoringElixir do
+  @moduledoc """
+  Documentation for `UptimeMonitoringElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UptimeMonitoringElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uptime_monitoring_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule UptimeMonitoringElixirTest do
+  use ExUnit.Case
+  doctest UptimeMonitoringElixir
+
+  test "greets the world" do
+    assert UptimeMonitoringElixir.hello() == :world
+  end
+end
